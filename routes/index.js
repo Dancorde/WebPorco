5fe81a1052329c9465092960b372bec1265ab545
Danilo Cordeiro <dancorde@gmail.com>
2018-06-17 18:08:26 -0300

    Pagamento OK

--- routes/index.js
+++ routes/index.js
@@ -39,4 +39,21 @@ router.get('/shopping-cart', function(req, res, next) {
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
+router.get('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', {products: null});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {total: cart.totalPrice});
+});
+
+router.post('/checkout', function(req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  req.session.cart = null;
+  res.redirect('/');
+});
+
 module.exports = router;
